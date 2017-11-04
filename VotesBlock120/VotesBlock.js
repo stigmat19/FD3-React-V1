c2da5b855f706fc8faa0e4121ef14695472cd03c
loktevalexey <loktev.alex.74@gmail.com>
2017-11-04 19:01:18 +0300

    -VotesBlock130

---
+++ VotesBlock120/VotesBlock.js
@@ -0,0 +1,19 @@
+﻿var VotesBlock = React.createClass({
+
+  displayName: 'VotesBlock',
+
+  render: function(){
+
+    var answersCode=this.props.info.answers.map( v =>
+        React.DOM.div({key:v.code,className:'Answer'},
+          React.DOM.span({className:'Count'},v.count),
+          React.DOM.span({className:'Text'},v.text),
+        )
+      );
+    return React.DOM.div( {className:'VotesBlock'}, 
+      React.DOM.div( {className:'Question'}, this.props.info.question ),
+      React.DOM.div( {className:'Answers'}, answersCode ),
+    );
+  },
+
+});
