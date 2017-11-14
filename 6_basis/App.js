ec38ab0d36a82912e372b80c6b657394044a23a1
loktevalexey <loktev.alex.74@gmail.com>
2017-11-14 13:10:37 +0300

    examples ready

---
+++ 6_basis/App.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TestComponent from './components/TestComponent';
+
+ReactDOM.render( 
+  <TestComponent />
+, document.getElementById('container') );
